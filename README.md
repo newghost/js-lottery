d574c940432d4b6cceebfc369e46170745781900
Kris Zhang <c2u@live.cn>
2013-11-24 17:10:28 +0800

    Update gh-pages
    update

---
+++ README.md
@@ -0,0 +1,19 @@
+Photo Lottery
+=========================================
+A lottery application, images/photos slider, written in JavaScript, it's very simple and need not to be installed. Play as photos gallery and slide these images quickly, stop at a random position. It can be used  in party, lucky draw etc.
+
+Support: IE9+, FF, Chrome
+
+
+Please find [DEMO](http://newghost.github.com/js-lottery/lottery.html)
+
+[DEMO lucky draw by phone number](http://newghost.github.com/js-lottery/lottery_phone.html)
+-----------------------------------------
+
+
+Disclaimer: All materials got from the Internet, if infringement, please inform me immediately.
+
+新增按手机号抽奖，只需要更新phone.txt文件即可。
+
+
+PS: 照片轮转滚动抽奖程序，使用JS编写，无需安装，适合年会使用。 所有图片、素材均来自互联网，如有侵权，请即时告知。
---
+++ res/lottery_phone.js
@@ -0,0 +1,128 @@
+/*
+* Photo Lottery
+* Author: Kris Zhang
+* Lincense: MIT
+* https://github.com/newghost/js-lottery.git
+*/
+/*
+Fix old IE.
+*/
+var Audio = Audio || function() { 
+  var self  = this;
+  self.play = self.stop = new Function();
+};
+
+
+var Lottery = (function() {
+
+  var timer           = null,
+      itemWidth       = 142,
+      itemCount       = 0,
+      curPos          = 0;
+
+  var stopAudio       = new Audio("res/ping.mp3");
+
+  var $container      = $("#lottery-container"),
+      $content        = $("#lottery-container ul"),
+      $item,
+      $hero           = $("#lottery-hero span");
+
+  var init = function() {
+
+    $.ajax({
+        url: $container.data("url")
+      , dataType: "text"
+      , success: function(data) {
+        var phones = data.toString().split('\r\n');
+
+        for (var i = 0; i < phones.length; i++) {
+          var phone = phones[i];
+          phone && $content.append("<li><b>" + phone + "</b></li>");
+        }
+
+        $item           = $("#lottery-container ul li"),
+
+        //Pre-caculate the count of items
+        itemCount       = $item.size();
+        //Clone the contents
+        $content.append($content.html());
+      }
+    });
+
+  };
+
+  var start  = function() {
+    clearInterval(timer);
+
+    timer = setInterval(function() {
+
+      curPos = parseInt($content.css("left")) | 0;
+      curPos -= itemWidth / 2;
+
+      (curPos < 0 - itemWidth * itemCount) && (curPos = 0);
+
+      $content.css("left", curPos);
+
+    }, 25);
+
+    $hero.hide();
+  };
+
+  var stop = function() {
+    clearInterval(timer);
+    timer = null;
+
+    stopAudio.play();
+
+    //Roll at the half width?
+    (curPos % itemWidth == 0 - itemWidth / 2) && (curPos = curPos - itemWidth / 2);
+
+    var selected  = getCurIdx();
+
+    setCurIdx(selected);
+  };
+
+  var running = function() {
+    return timer != null;
+  };
+
+  //Index: first item on the left
+  var setCurIdx = function(idx) {
+    curPos = (0 - idx) * itemWidth;
+
+    var $items = $("#lottery li b"),
+        phone  = $items.eq(idx + 3).html();
+
+    $content.css("left", curPos);
+    $hero.html(phone).show("slow");
+
+    console.log(curPos, idx);
+  };
+
+  var getCurIdx = function() {
+    return (0 - curPos) / itemWidth;
+  };
+
+  return {
+      init: init
+    , start: start
+    , stop: stop
+    , running: running
+    , setCurIdx: setCurIdx
+    , getCurIdx: getCurIdx
+  };
+
+})();
+
+$(document).ready(function() {
+  Lottery.init();
+});
+
+$(document).keydown(function(e) {
+    var key = e.keyCode;
+    if (key != 32 && key != 13) return;
+
+    Lottery.running()
+      ? Lottery.stop()
+      : Lottery.start();
+});
